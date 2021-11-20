4ef1b019667977c2902f5a3c9139ffe162c9f101
Ishanh2000 <misraishanh200@gmail.com>
2021-11-21 01:00:55 +0530

    Added flip_flop data

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,8 @@
+# AUM SHREEGANESHAAYA NAMAH|| AUM SHREEHANUMATE NAMAH||
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import os
 
 DATA_DIR = "raw_data/"
 
@@ -43,7 +46,54 @@ def plot_acc_data(filename, show_plot=False):
     return timestamps, acc_total, (acc_x, acc_y, acc_z), (total_plot, axes_plot)
 
 
-# plot_acc_data("fwd_bkwd.csv", show_plot=True)
-# plot_acc_data("wave.csv", show_plot=True)
-plot_acc_data("cross.csv", show_plot=True)
-plt.show()
+def plot_all_data(fname):
+    f = open(fname)
+    allRows = list(csv.reader(f))
+    headers, rows = allRows[0], np.array([ [ float(x) for x in row ] for row in allRows[1:]])
+    dims = len(headers)-1 # total dimensions of movements
+    plt.figure()
+    zeroLine = [0 for row in rows]
+    for i in range(dims):
+        # if i != 4: continue
+        plt.subplot(2, 3, i+1)
+        plt.plot(rows[:,(i+1)])
+        plt.plot(zeroLine)
+        plt.ylabel(headers[i+1])
+    plt.show()
+    f.close()
+    
+
+def split_data(name, arr):
+    f = open(f"../raw_data/{name}.csv")
+    allRows = f.readlines()
+    headers, rows = allRows[0], allRows[1:]
+    for i in range(len(arr)-1):
+        if not os.path.isdir(f"../raw_templates/{name}"): os.mkdir(f"../raw_templates/{name}")
+        fout = open(f"../raw_templates/{name}/{i+1}.csv", "w")
+        fout.write(headers)
+        for j in range(arr[i], arr[i+1]+1): fout.write(rows[j]) # now write data points
+        fout.close()
+    f.close()
+
+
+
+if __name__ == "__main__":
+    # plots originally done by Ashwin
+    # plot_acc_data("fwd_bkwd.csv", show_plot=True)
+    # plot_acc_data("wave.csv", show_plot=True)
+    # plot_acc_data("cross.csv", show_plot=True)
+    # plt.show()
+
+    # additions by Ishanh
+    plot_all_data(f"../raw_data/flip_flop.csv")
+    # split_data("flip_flop", [
+    #       53,  200,  337,  473,  602,  729,  875,  997, 1116, 1243, 1373, 1511, 1651, 1786, 1920, 2040, 2175,
+    #     2311, 2443, 2570, 2715, 2834, 2979, 3091, 3226, 3353, 3489, 3613, 3735, 3882, 4024, 4141, 4277
+    # ])
+    for i in range(16): plot_all_data(f"../raw_templates/flip/{1+i}.csv")
+    for i in range(16): plot_all_data(f"../raw_templates/flop/{1+i}.csv")
+    
+    # 
+
+
+# with intervals, 15 copies each of up_down, flip_flop, bow, left_right
